dd874600d5e1cdfe19e24cb17c35be772f0669b0
victoriak94 <vkamila6@gmail.com>
2019-02-12 01:40:05 -0500

    Add burger animation on toggle

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
+
+        // Burger animation
+        burger.classList.toggle('toggle');
     });
 }
 
